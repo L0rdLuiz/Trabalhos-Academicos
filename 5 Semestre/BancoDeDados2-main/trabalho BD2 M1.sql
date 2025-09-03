9da9b756dd854290a09652a758395e14e80f9432
Luiz Antonio <101644276+L0rdLuiz@users.noreply.github.com>
2025-09-02 21:01:25 -0300

    Refactor SQL tables and insert sample data

--- 5 Semestre/BancoDeDados2-main/trabalho BD2 M1.sql
+++ 5 Semestre/BancoDeDados2-main/trabalho BD2 M1.sql
@@ -1,6 +1,5 @@
 CREATE TABLE Livros (
-	ID serial primary key, 
-	id_livro int, 
+	ID_livro serial primary key, 
 	titulo text, 
 	autor text, 
 	preco numeric(6,2), 
@@ -8,34 +7,62 @@ CREATE TABLE Livros (
 );
 
 CREATE TABLE Clientes (
-	ID serial primary key,
-	id_cliente int,
+	ID_cliente serial primary key,
 	nome text,
 	email varchar(255)
 );
 
 CREATE TABLE Pedidos (
-	ID serial primary key,
-	id_pedido int,
-	id_cliente int,
+	ID_pedido serial primary key,
+	ID_cliente int,
 	data_pedido TIMESTAMPTZ NOT NULL DEFAULT now(),
 	status VARCHAR(50) NOT NULL DEFAULT 'pendente'
 );
 
 CREATE TABLE Itens_pedido (
-	ID serial primary key,
-	id_item int,
-	id_pedido int,
-	id_livro int,
+	ID_item serial primary key,
+	ID_pedido int,
+	ID_livro int,
 	quantidade int,
 	preco_unitario numeric(6,2)
 );
 
 CREATE TABLE Log_auditoria (
-	ID serial primary key,
-	id_log int,
+	ID_log serial primary key,
 	tabela_afetada VARCHAR(100),
-	id_registro_afetado int,
+	ID_registro_afetado int,
 	operacao VARCHAR(100)
 );
 
+INSERT INTO Livros (titulo, autor, preco, quant_estoque)
+values ('A Arte da Guerra', 'Sun Tzu', 12.49, 10),
+	   ('Extraordinário', 'R. J. Palacio', 39.99, 25),
+	   ('O cavaleiro preso na armadura', 'Robert Fischer', 32.77, 6),
+	   ('Diário de um Banana I', 'Jeff Kinney', 48.90, 55)
+	   
+INSERT INTO Clientes (nome, email)
+values ('Luiz', 'Luiz@gmail.com'),
+	   ('Isabela', 'Isabela@gmail.com'),
+	   ('Carlos', 'Carlos@gmail.com'),
+	   ('Felype', 'Felype@gmail.com')
+	   
+INSERT INTO Pedidos (ID_cliente)
+values (1)
+INSERT INTO Pedidos (ID_cliente)
+values (2)
+INSERT INTO Pedidos (ID_cliente)
+values (3)
+INSERT INTO Pedidos (ID_cliente)
+values (4)
+
+INSERT INTO Itens_pedido (ID_pedido, ID_livro, quantidade, preco_unitario)
+values (1, 3, 2, 32.77),
+	   (2, 1, 1, 12.49),
+	   (3, 4, 1, 48.90),
+	   (4, 2, 1, 39.99),
+	   (5, 1, 2, 12.49)
+	   
+select * from Livros;
+select * from Clientes;
+select * from Pedidos;
+select * from Itens_pedido;
